af3ffabc7c3c70c6c9d2b46095780b2d5f215f8e
David Jimenez <djosuejj@gmail.com>
2018-08-30 22:46:39 -0500

    Trabajo Clase Jueves 30-08-2018

--- clase11-ejercicios/ejemplos-tuiter/run2.py
+++ clase11-ejercicios/ejemplos-tuiter/run2.py
@@ -1,5 +1,7 @@
 from credenciales import *
 import tweepy
+import codecs
+
 # https://tweepy.readthedocs.io/en/v3.5.0/
 
 # Access and authorize our Twitter credentials from credentials.py
@@ -8,7 +10,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 # For loop to iterate over tweets with #ocean, limit to 10
-lista = ["usuarios-tuiter"]
+lista = ['CarlosC62169047', 'djosuejj', 'pedrojsalinas', 'fernandogdo1']
 for l in lista:
-    user_data = api.get_user(l) 
-    print "%s - %s" % (l, user_data['created_at'])
+    archivo = codecs.open('%s.csv' %l, 'w', encoding='utf-8')
+    informacion = api.followers(l)
+    for x in informacion['users']:
+    	datos = {'followers_count': x['followers_count'], 'listed_count': x['listed_count'], 'friends_count': x['friends_count'],
+    	'screen_name': x['screen_name'], 'favourites_count': x['favourites_count'], 'name': x['name'], 'created_at': x['created_at'] }
+    	archivo.write('{name}|{screen_name}|{followers_count}|{friends_count}|{listed_count}|{favourites_count}|\n'.format(** datos))
+archivo.close()
+
